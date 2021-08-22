2e16d2126dd7e588d627e020976089a48c4fef7b
furbox <furbox@gmail.com>
2021-08-22 03:33:32 -0500

    event bus and query

--- client/src/PostList.jsx
+++ client/src/PostList.jsx
@@ -7,7 +7,7 @@ function PostList() {
     const [posts, setPosts] = useState({});
 
     const fetchPosts = async () => {
-        const res = await axios.get('http://localhost:4002/posts');
+        const res = await axios.get('http://localhost:4000/posts');
         setPosts(res.data);
     }
 
