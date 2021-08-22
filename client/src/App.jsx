c0c935bd5d4dbee7a12bcf0ca494aa16adab9ba5
furbox <furbox@gmail.com>
2021-08-21 23:44:47 -0500

    add posts client

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import PostCreate from "./PostCreate";
+
+const App = () => {
+    return (
+        <div className="container">
+            <h1>Blog app</h1>
+            <PostCreate />
+        </div>
+    )
+}
+
+export default App;
