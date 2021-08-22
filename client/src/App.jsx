0d58f68eb67a7c555ca241d87070dcba111beef0
furbox <furbox@gmail.com>
2021-08-22 01:16:55 -0500

    add comment  client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import PostCreate from "./PostCreate";
+import PostList from "./PostList";
 
 const App = () => {
     return (
         <div className="container">
             <h1>Blog app</h1>
             <PostCreate />
+            <hr />
+            <PostList/>
         </div>
     )
 }
